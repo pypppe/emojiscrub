f3762553267ddc38564fc07497e48c1e1c7f98ae
lily <py@astrarune.com>
2025-12-18 17:29:27 +0000

    Update script.js

--- exeapp/script.js
+++ exeapp/script.js
@@ -4,8 +4,13 @@ button.addEventListener("click", () => {
     if (button.dataset.cooldown) return;
 
     const originalText = button.textContent;
-    button.textContent = "Coming Soon!";
+    button.textContent = "Taking you to GitHub releases...";
     button.dataset.cooldown = "true";
+
+    window.open(
+        "https://github.com/pypppe/emojiscrub/releases",
+        "_blank"
+    );
 
     setTimeout(() => {
         button.textContent = originalText;
