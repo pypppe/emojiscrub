31e7505363942277e41a1168ca3b833d2dbff48e
kona-chan <py@astrarune.com>
2026-02-13 22:04:38 +0000

    Random ass strings added

--- zandovo.js
+++ zandovo.js
@@ -17,7 +17,7 @@
     }, 400);
 
     setTimeout(() => {
-        replaceQuery("ZANDOVO_TRUE");
+        replaceQuery("ZANDOVO_TRUE_U4Do7QTFO7uhD9lke7T4h5MvuB3foy3d_UecA1Hj7I9QAwo2CczGnVQ6KXQeIJSyueH3tbuAVNONYjYK8TfrsctVSOcZoy36BKKae9VAD8SbS5SYqryLvFIpo857RXxj8y6aYulX8UFWsSREJdCkFIHPlqzPwQ7ayta2syt0Y42D6L4Ts9xbRI46qN0H54mT4_TUCxcAe11A2UTl6bsp9Z0tYMmOizjWvk");
     }, 500);
 
     setTimeout(() => {
