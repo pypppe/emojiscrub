14096908f41d8890377bc871e0b97e30bc1696bc
kona-chan <py@astrarune.com>
2026-02-13 22:53:00 +0000

    Make the setTimeout time lower

--- zandovo.js
+++ zandovo.js
@@ -18,7 +18,7 @@
 
     setTimeout(() => {
         replaceQuery("ZANDOVO_TRUE_U4Do7QTFO7uhD9lke7T4h5MvuB3foy3d_UecA1Hj7I9QAwo2CczGnVQ6KXQeIJSyueH3tbuAVNONYjYK8TfrsctVSOcZoy36BKKae9VAD8SbS5SYqryLvFIpo857RXxj8y6aYulX8UFWsSREJdCkFIHPlqzPwQ7ayta2syt0Y42D6L4Ts9xbRI46qN0H54mT4_TUCxcAe11A2UTl6bsp9Z0tYMmOizjWvk");
-    }, 1000);
+    }, 500);
 
     setTimeout(() => {
         replaceQuery("ZANDOVOUK_PACKAGE366_RG22XPPikpxT7XoQZrfCMsL6uhvVliDff1hOZTMISwTcDKf2EF97eQX");
