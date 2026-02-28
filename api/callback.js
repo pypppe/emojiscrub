2a12d992a697bfcc3f0b798866454da3c93fce57
kona-chan <py@astrarune.com>
2026-02-28 18:43:15 +0000

    Add Discord OAuth2 callback handler

---
+++ api/callback.js
@@ -0,0 +1,35 @@
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.redirect('/?error=no_code');
+  }
+
+  const tokenRes = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: 'https://escrub.astrarune.com/api/callback',
+    }),
+  });
+
+  const tokenData = await tokenRes.json();
+
+  if (!tokenData.access_token) {
+    return res.redirect('/?error=token_failed');
+  }
+
+  const userRes = await fetch('https://discord.com/api/users/@me', {
+    headers: { Authorization: `Bearer ${tokenData.access_token}` },
+  });
+
+  const user = await userRes.json();
+
+  const avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+
+  res.redirect(`/?discord_id=${user.id}&username=${encodeURIComponent(user.username)}&avatar=${encodeURIComponent(avatar)}`);
+}
